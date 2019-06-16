3f4c9678bed754ed63dbf0dd51f60203385d441a
isaranov <isaranov@redhat.com>
2019-06-16 11:26:32 +0200

    New github bot

---
+++ github_bot/__main__.py
@@ -0,0 +1,9 @@
+from octomachinery.app.server.runner import run as run_app
+
+
+if __name__ == "__main__":
+    run_app(
+        name='isaranova-pycon-bot-workshop',
+        version='1.0.0',
+        url='https://isaranova-pycon-bot.herokuapp.com/',
+    )
